eed93e467e6c5a4c6d8d473394e759c79fdd7979
linode <leotisbuchanan@gmail.com>
2015-05-28 01:16:43 +0000

    add command_to_start_spark_ipython

--- code/exploratory_data.py
+++ code/exploratory_data.py
@@ -6,3 +6,11 @@ df = sqlContext.jsonFile("twitter_stream.20150519-084907.json")
 df.show()
 # Print the schema in a tree format
 df.printSchema()
+#get all the text of the tweets
+# Register the DataFrame as a table.
+df.registerTempTable("tweet")
+results = sqlContext.sql("SELECT id, text FROM tweet")
+results_with_text = results.filter(lambda p: p.text not None)
+text = results.map(lambda p: p.text)
+for t in text.collect():
+  print t
