984460d342509ce42e7dab8a2458bf2140fa1083
LeotisBuchanan <LeotisBuchanan@gmail.com>
2015-06-17 10:01:53 -0400

    Update trainNaiveandCreateNaiveBayesModel.py
    add naive bayes training code, need to make it compile and run

--- code/naive_bayes_tweet_classifier/trainNaiveandCreateNaiveBayesModel.py
+++ code/naive_bayes_tweet_classifier/trainNaiveandCreateNaiveBayesModel.py
@@ -24,6 +24,27 @@ INPUT_LABEL_TWEETS_DATA_PATH = "data/input/labeledtweets"
 NAIVE_BAYES_MODEL_PATH = "model/naivebayes"
 
 
+from pyspark.mllib.classification import NaiveBayes
+from pyspark.mllib.linalg import Vectors
+from pyspark.mllib.regression import LabeledPoint
+
+def parseLine(line):
+    parts = line.split(',')
+    label = float(parts[0])
+    features = Vectors.dense([float(x) for x in parts[1].split(' ')])
+    return LabeledPoint(label, features)
+
+data = sc.textFile('data/mllib/sample_naive_bayes_data.txt').map(parseLine)
+
+# Split data aproximately into training (60%) and test (40%)
+training, test = data.randomSplit([0.6, 0.4], seed = 0)
+
+# Train a naive Bayes model.
+model = NaiveBayes.train(training, 1.0)
+
+# Make prediction and test accuracy.
+predictionAndLabel = test.map(lambda p : (model.predict(p.features), p.label))
+accuracy = 1.0 * predictionAndLabel.filter(lambda (x, v): x == v).count() / test.count()
 
 def generatedHashedFeatures(tweet):
     text = "test text Hashing term frequency vectorizer with 50k features"
